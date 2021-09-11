baa579b2245b3deede1938f01d3d91bd6b9df7ee
rpal <rpal@rocketsoftware.com>
2021-09-11 15:54:15 +0530

    test

--- src/__tests__/MediaCard.test.js
+++ src/__tests__/MediaCard.test.js
@@ -1,27 +1,7 @@
 import { render } from '@testing-library/react';
 import MediaCard from '../components/MediaCard/MediaCard';
 import {create, act} from 'react-test-renderer'; 
-import Card from '@material-ui/core';
-
 test('renders MediaCard component', () => {
-    const props = {
-        user: {
-            picture: {
-                large: 'abc.png',
-            },
-            name: {
-                first: 'first',
-                last: 'last'
-            },
-            email: 'abc@test.com',
-            phone: '1234565',
-            cell: '1234567890'
-        }
-    }
-    render(<MediaCard {...props} />);
-});
-
-test('Card component renders inside MediaCard component', () => {
     const props = {
         user: {
             picture: {
